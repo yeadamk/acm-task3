f1327536aeacae9092b092271bc9d73be3c155d4
Yeadam Kim <yes752044@gmail.com>
2024-02-12 20:21:31 -0800

    improved css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import logo from "./logo.png";
-import "./App.css";
+import "./app.css";
 import Post from "./Post";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 import { IoMdAddCircle } from "react-icons/io";
@@ -24,7 +24,7 @@ function App() {
   };
 
   const handleScroll = () => {
-    const targetElement = document.querySelector(".Blog")
+    const targetElement = document.querySelector(".blog")
     if (targetElement) {
       window.scrollTo(0, targetElement.offsetTop)
     }
@@ -47,19 +47,27 @@ function App() {
         <IoIosArrowDropdownCircle className="dropdown" onClick={handleScroll}/>
       </header>
       <section className="blog" id="blog">
-        <div className="post-container">
-          {posts.map(post => (
-              <Post key={post.id} post={post} />
-          ))}
 
-          <form className="form-container" onSubmit={handleFormSubmit}>
-            <input type="text" name="title" placeholder="Post Title" required />
-            <textarea name="content" placeholder="Post Content" required></textarea>
-            <button type="submit">Add Post</button>
-          </form>
+        <div className="post-container">
+          {posts.length > 0 ? (
+              posts.map(post => (
+                <Post key={post.id} post={post} />
+              ))
+            ) : (
+              <div>No posts available</div>
+            )}
         </div>
+
+        <form className="form-container" onSubmit={handleFormSubmit}>
+          <input type="text" name="title" placeholder="Post Title" required />
+          <textarea name="content" placeholder="Post Content" required></textarea>
+          <button type="submit">Add Post</button>
+        </form>
+
         <IoMdAddCircle className="addPost" onClick={addPost}/>
+
       </section>
+      <footer><span>© 2024 Yeadam Kim</span></footer>
     </main>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -5,11 +5,11 @@ const Post = ({ post }) => {
     const [likes, setLikes] = useState(post.likes);
 
     return (
-        <section className="post">
+        <div className="post">
             <h2 className="post-title">{post.title}</h2>
             <p className="post-content">{post.content}</p>
             <button className="like-button" onClick={() => setLikes(likes + 1)}>Like ({likes})</button>
-        </section>
+        </div>
     );
 };
 
